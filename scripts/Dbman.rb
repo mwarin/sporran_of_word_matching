23e8370beabdd0d744445650aaa9a116c37fcf02
Martin Warin <mwarin@umich.edu>
2019-04-10 11:39:26 -0400

    using built-in dotenv functionality for required config items instead

--- scripts/Dbman.rb
+++ scripts/Dbman.rb
@@ -9,13 +9,7 @@ Reads config from closest .env and sets up a MySQL connection.
 =end
 
 class Dbman
-  @@required = %w[db_host db_user db_pw db_port db_port];
-  @@required.each do |r|
-    if !ENV[r] then
-      raise "Required config item '#{r}' missing from nearest .env file.";
-    end
-  end
-
+  Dotenv.require_keys(%w[db_host db_user db_pw db_port db_port]);
   Mysql2::Client.default_query_options.merge!(:symbolize_keys => true);
   attr_reader :dbh;
   def initialize      
