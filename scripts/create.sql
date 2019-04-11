780d0b32f8b871a482ede27034b2722ee4347f17
Martin Warin <mwarin@umich.edu>
2019-04-11 13:08:24 -0400

    hash index instead of btree

--- scripts/create.sql
+++ scripts/create.sql
@@ -3,5 +3,8 @@ DROP TABLE IF EXISTS ht_oclc_bow;
 CREATE TABLE ht_oclc_title(oclc INTEGER PRIMARY KEY, title VARCHAR(250));
 CREATE TABLE ht_oclc_bow(oclc INTEGER, word VARCHAR(50), stop TINYINT NOT NULL DEFAULT 0, PRIMARY KEY (oclc, word));
 
-CREATE INDEX ht_oclc_bow_word USING BTREE ON ht_oclc_bow (oclc);
-CREATE INDEX ht_oclc_title_title USING BTREE ON ht_oclc_title (title); 
+-- DROP INDEX ht_oclc_bow_word ON ht_oclc_bow;
+-- DROP INDEX ht_oclc_title_title ON ht_oclc_title;
+
+CREATE INDEX ht_oclc_bow_word    USING HASH ON ht_oclc_bow (oclc);
+CREATE INDEX ht_oclc_title_title USING HASH ON ht_oclc_title (title); 
