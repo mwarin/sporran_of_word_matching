44c697c6a54b948597d706cc11c85c2823bf7380
Martin Warin <mwarin@umich.edu>
2019-04-11 10:30:41 -0400

    more index

--- scripts/create.sql
+++ scripts/create.sql
@@ -3,4 +3,5 @@ DROP TABLE IF EXISTS ht_oclc_bow;
 CREATE TABLE ht_oclc_title(oclc INTEGER PRIMARY KEY, title VARCHAR(250));
 CREATE TABLE ht_oclc_bow(oclc INTEGER, word VARCHAR(50), stop TINYINT NOT NULL DEFAULT 0, PRIMARY KEY (oclc, word));
 
-CREATE INDEX ht_oclc_bow_word USING BTREE ON ht_oclc_bow (oclc); 
+CREATE INDEX ht_oclc_bow_word USING BTREE ON ht_oclc_bow (oclc);
+CREATE INDEX ht_oclc_title_title USING BTREE ON ht_oclc_title (title); 
