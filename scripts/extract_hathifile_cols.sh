df1bc5151ed475de9da573046118578cef48495e
Martin Warin <mwarin@umich.edu>
2019-04-11 10:33:46 -0400

    passing hathifile as arg

--- scripts/extract_hathifile_cols.sh
+++ scripts/extract_hathifile_cols.sh
@@ -1,2 +1,2 @@
-hathifile='/htapps/mwarin.babel/phdb_scripts/data/builds/current/hathi_full.txt';
-head -100000 $hathifile | cut -f8,12 | sort -u > data/hathifile_extract_cols.tsv;
+hathifile=$1;
+cut -f8,12 $hathifile | sort -u > data/hathifile_extract_cols.tsv;
