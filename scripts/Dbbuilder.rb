dcd18bebc7d7c37539c21c455b56a24c807bf3dc
Martin Warin <mwarin@umich.edu>
2019-04-15 11:57:20 -0400

    added middle table for words

--- scripts/Dbbuilder.rb
+++ scripts/Dbbuilder.rb
@@ -4,28 +4,76 @@ require 'i18n';
 I18n.available_locales = [:en];
 
 class Dbbuilder
-
+  attr_reader :word_cache, :cache_hit, :cache_miss;
   def initialize
     @dbh = Dbman.new.dbh;
+    @word_cache      = {};
+    @word_cache_max  = 20000;
+    @get_word_id_sth = @dbh.prepare("SELECT word_id FROM ht_word WHERE word = ?");
+    @set_word_id_sth = @dbh.prepare("INSERT INTO ht_word (word) VALUES (?)");
+    @get_last_id_sth = @dbh.prepare("SELECT LAST_INSERT_ID() AS id");
+    @cache_hit  = 0;
+    @cache_miss = 0;
+    @cache_full = 0;
   end
 
   def clean
-    %w[ht_oclc_title ht_oclc_bow].each do |t|
+    %w[ht_oclc_title ht_oclc_bow ht_word].each do |t|
       puts "Truncating #{t}";
       q = @dbh.prepare("TRUNCATE TABLE #{t}");
       q.execute();
     end
   end
 
+  def get_word_id (word)
+    # memoized
+    if @word_cache.key?(word) then
+      @cache_hit += 1;
+      @word_cache[word][:freq] += 1;
+      return @word_cache[word][:val];
+    end
+    @cache_miss += 1;
+
+    word_id = nil;
+    res = @get_word_id_sth.execute(word);
+    res.each do |row|
+      word_id = row[:word_id];
+    end
+    res.free;
+    
+    if word_id.nil? then
+      @set_word_id_sth.execute(word);
+      word_id = @set_word_id_sth.last_id;
+    end
+    if word_id.nil? then
+      raise "unable to find word_id for #{word}";
+    end
+    @word_cache[word] = {:val => word_id, :freq => 1};
+
+    # When cache is full, throw out the least frequent 1/5th.
+    if @word_cache.keys.size > @word_cache_max then
+      @cache_full += 1;
+      @word_cache.sort_by{|k,v| v[:freq]}.first(@word_cache_max / 5).each do |k,v|
+        @word_cache.delete(k);
+      end
+    end
+    
+    return word_id;
+  end
+  
   def load (hathifile)
     inf    = File.open(hathifile, 'r');
     outf_t = File.open('./oclc_title.tsv', 'w');
     outf_w = File.open('./oclc_word.tsv', 'w');
+
     # Get oclc and title from each line in hathifile
     i = 0;
     inf.each_line do |line|
       i += 1;
-      puts i if i % 5000 == 0;
+      if i % 10000 == 0 then
+        puts "#{i} #{Time.new} cache size #{@word_cache.keys.size}, cache full #{@cache_full} times, cache hit/miss #{@cache_hit}/#{@cache_miss}";
+      end
+
       cols    = line.split("\t");
       oclc_t  = cols[0];
       oclc_t.split(',').each do |oclc| # could be multiple oclcs
@@ -33,7 +81,7 @@ class Dbbuilder
         outf_t.puts([oclc, title].join("\t"));
           words     = title.downcase.split(' ').map{|x| x.gsub(/[^a-z]/, '')}.select{|x| x =~ /[a-z]/}.uniq;
           words.each do |w|
-            outf_w.puts([oclc, w].join("\t"));
+            outf_w.puts([oclc, get_word_id(w)].join("\t"));
           end
       end
     end
@@ -41,12 +89,19 @@ class Dbbuilder
     [inf, outf_w, outf_t].each{|f| f.close()};
     puts "loading files into tables";
     @dbh.query("LOAD DATA LOCAL INFILE '#{File.expand_path(outf_t)}' IGNORE INTO TABLE ht_oclc_title (oclc, title)");
-    @dbh.query("LOAD DATA LOCAL INFILE '#{File.expand_path(outf_w)}' IGNORE INTO TABLE ht_oclc_bow (oclc, word)");
+    @dbh.query("LOAD DATA LOCAL INFILE '#{File.expand_path(outf_w)}' IGNORE INTO TABLE ht_oclc_bow (oclc, word_id)");
   end
-  end
+end
 
 if $0 == __FILE__ then
-  dbb = Dbbuilder.new();
-  dbb.clean();
-  dbb.load('/htapps/mwarin.babel/sporran_of_word_matching/data/hathifile_extract_cols.tsv');
+  if ARGV[0].nil? then
+    raise "Pass path to hathi-oclc-title.tsv file as 1st arg";
+  else
+    puts Time.new;
+    dbb = Dbbuilder.new();
+    dbb.clean();
+    dbb.load(ARGV[0]);
+    puts Time.new;
+    puts "cache hit/miss = #{dbb.cache_hit} / #{dbb.cache_miss}";
+  end
 end
--- scripts/create.sql
+++ scripts/create.sql
@@ -1,10 +1,14 @@
 DROP TABLE IF EXISTS ht_oclc_title;
+DROP TABLE IF EXISTS ht_word;
 DROP TABLE IF EXISTS ht_oclc_bow;
-CREATE TABLE ht_oclc_title(oclc INTEGER PRIMARY KEY, title VARCHAR(250)) ENGINE = MyISAM;
-CREATE TABLE ht_oclc_bow(oclc INTEGER, word VARCHAR(50), stop TINYINT NOT NULL DEFAULT 0, PRIMARY KEY (oclc, word)) ENGINE = MyISAM;
+
+CREATE TABLE ht_oclc_title(oclc INTEGER, title VARCHAR(250));
+CREATE TABLE ht_word(word_id INTEGER NOT NULL AUTO_INCREMENT, word VARCHAR(50) NOT NULL, stop TINYINT NOT NULL DEFAULT 0, PRIMARY KEY (word_id));
+CREATE TABLE ht_oclc_bow(oclc INTEGER, word_id INTEGER, PRIMARY KEY (oclc, word_id));
 
 -- DROP INDEX ht_oclc_bow_word ON ht_oclc_bow;
 -- DROP INDEX ht_oclc_title_title ON ht_oclc_title;
 
-CREATE INDEX ht_oclc_bow_word    USING HASH ON ht_oclc_bow (word);
-CREATE INDEX ht_oclc_title_title USING HASH ON ht_oclc_title (title); 
+CREATE INDEX ht_word_word        USING BTREE ON ht_word (word);
+CREATE INDEX ht_oclc_bow_word_id USING BTREE ON ht_oclc_bow (word_id);
+CREATE INDEX ht_oclc_title_title USING BTREE ON ht_oclc_title (title); 
