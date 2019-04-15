9f0df542f46d95158b871566fbdbbe5af77a137a
Martin Warin <mwarin@umich.edu>
2019-04-15 17:49:14 -0400

    mostly cosmetic

--- scripts/Matcher.rb
+++ scripts/Matcher.rb
@@ -22,34 +22,29 @@ class Matcher
     @mode = mode;
     @dbh = Dbman.new.dbh;
     @query_cache = {};
-
-
     get_full_title_by_oclc_sql = 'SELECT title FROM ht_oclc_title WHERE oclc = ?';
     @get_full_title_by_oclc    = @dbh.prepare(get_full_title_by_oclc_sql);
-
     @stop = Stopword.new.get_list();
   end
 
   def get_words_oclc_q (search_words)
     # Caches query for variable number of WHERE-args and proportional HAVING
-    if @query_cache.key?(search_words.size) then
-      return @query_cache[search_words.size];
+    if !@query_cache.key?(search_words.size) then
+      qmarks = (['?'] * search_words.size).join(',');
+      # if there are e.g. 8 search_words then require at least 8/2 in the HAVING.
+      sql    = %W[
+        SELECT b.oclc, COUNT(b.word_id) AS c, GROUP_CONCAT(w.word) AS words
+        FROM ht_oclc_bow AS b
+        JOIN ht_word     AS w ON (b.word_id = w.word_id)
+        WHERE w.stop = 0 AND w.word IN (#{qmarks})
+        GROUP BY b.oclc HAVING c > #{search_words.size / 2}
+      ].join(' ');
+      puts "caching query (@query_cache[#{search_words.size}]): #{sql}";
+      q = @dbh.prepare(sql);
+      @query_cache[search_words.size] = q;
     end
 
-    qmarks = (['?'] * search_words.size).join(',');
-    # if there are e.g. 8 search_words then require at least 8/2 in the HAVING.
-    sql    = %W[
-      SELECT b.oclc, COUNT(b.word_id) AS c, GROUP_CONCAT(w.word) AS words
-      FROM ht_oclc_bow AS b
-      JOIN ht_word     AS w ON (b.word_id = w.word_id)
-      WHERE w.stop = 0 AND w.word IN (#{qmarks})
-      GROUP BY b.oclc HAVING c > #{search_words.size / 2}
-    ].join(' ');
-    puts "caching query (@query_cache[#{search_words.size}]): #{sql}";
-    q = @dbh.prepare(sql);
-    @query_cache[search_words.size] = q;
-
-    return q;
+    return @query_cache[search_words.size];
   end
 
   def look_up_title (search_title)
@@ -65,6 +60,7 @@ class Matcher
     # get a (cached) query with the right number of WHERE-args and proportional HAVING COUNT(x).
     get_oclcs_q = get_words_oclc_q(search_title_words);
 
+    # get all ocns and their associated words based on search title
     res = get_oclcs_q.execute(*search_title_words);
     res.each do |row|
       oclc = row[:oclc];
@@ -72,10 +68,10 @@ class Matcher
     end
     res.free;
 
-    search_title_wc = search_title_words.size;
     scores     = [];
-    low_score  = 0;
     oclc_title = {}
+
+    # for each oclc=>[words], compare against search_title and score
     oclc_words.each do |match_ocn, match_words|
       match_title = '';
       res = @get_full_title_by_oclc.execute(match_ocn);
@@ -85,18 +81,17 @@ class Matcher
         precision = match_words.size.to_f / search_title_words.size.to_f;
         recall    = match_words.size.to_f / (search_title_words + match_title_words).uniq.size.to_f;
         score     = (precision + recall)  / 2;
+        # Only bother if score is above min_score.
         if score >= @mode[:min_score] then
           scores << {
-            :oclc=>match_ocn,
-            :score=>score,
-            :title=>match_title,
-            :search_words=>search_title_words,
-            :match_words=>match_words,
-            :precision=>precision,
-            :recall=>recall
+            :oclc         => match_ocn,
+            :score        => score,
+            :title        => match_title,
+            :search_words => search_title_words,
+            :match_words  => match_words,
+            :precision    => precision,
+            :recall       => recall
           };
-        else
-          low_score += 1;
         end
       end
       res.free;
@@ -107,7 +102,6 @@ class Matcher
         puts "#{h[:score]}\tp#{h[:precision]}\tr#{h[:recall]}\t#{h[:oclc]}\t#{h[:search_words].join(',')}\t#{h[:match_words].join(',')}";
       end
       puts "Total matching: #{scores.size}";
-      puts "#{low_score} scored below minimum score of #{@mode[:min_score]}";
     end
     return scores;
   end
