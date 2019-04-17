44550e30fff3181485b47fb187df88e91bc63bc8
Martin Warin <mwarin@umich.edu>
2019-04-17 16:13:17 -0400

    towards collation

--- scripts/create.sql
+++ scripts/create.sql
@@ -6,6 +6,9 @@ CREATE TABLE ht_oclc_title(oclc INTEGER, title VARCHAR(250));
 CREATE TABLE ht_word(word_id INTEGER NOT NULL AUTO_INCREMENT, word VARCHAR(50) NOT NULL, stop TINYINT NOT NULL DEFAULT 0, PRIMARY KEY (word_id));
 CREATE TABLE ht_oclc_bow(oclc INTEGER, word_id INTEGER, PRIMARY KEY (oclc, word_id));
 
+ALTER TABLE ht_word       MODIFY word  VARCHAR(50)  CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE ht_oclc_title MODIFY title VARCHAR(250) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 -- DROP INDEX ht_oclc_bow_word ON ht_oclc_bow;
 -- DROP INDEX ht_oclc_title_title ON ht_oclc_title;
 
